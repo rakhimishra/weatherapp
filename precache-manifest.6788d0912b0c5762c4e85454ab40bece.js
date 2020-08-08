1c5a7c416a5ff27c120a30e8cd4bcf6fec393819
rakhimishra <mishrarakhi21@gmail.com>
2020-08-08 16:24:56 +0530

    Updates

---
+++ precache-manifest.6788d0912b0c5762c4e85454ab40bece.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7b8d12d62e9c31b6a15b1b38b9b217",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "6b37a3234e5848be2431",
+    "url": "/weatherapp/static/css/2.d039b2a6.chunk.css"
+  },
+  {
+    "revision": "d60d8cbbc818edf0da2d",
+    "url": "/weatherapp/static/css/main.d073db3d.chunk.css"
+  },
+  {
+    "revision": "6b37a3234e5848be2431",
+    "url": "/weatherapp/static/js/2.37842169.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weatherapp/static/js/2.37842169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60d8cbbc818edf0da2d",
+    "url": "/weatherapp/static/js/main.a828850d.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
